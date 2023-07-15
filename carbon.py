18fa3afcab548bc243eaf2ef43aa7e036a0da214
Mylo Cyrus <irahiggins740@gmail.com>
2023-07-15 13:55:55 -0700

    Add files via upload

--- carbon.py
+++ carbon.py
@@ -1,138 +1,247 @@
-←[38;2;0;10;255m←[0m
-←[38;2;0;25;255m←[0m
-←[38;2;0;40;255m←[0m
-←[38;2;0;55;255m←[0m
-←[38;2;0;70;255m                                  /$$$$$$   /$$$$$$  /$$$$$$$  /
-$$$$$$$   /$$$$$$  /$$   /$$←[0m
-←[38;2;0;85;255m                                 /$$__  $$ /$$__  $$| $$__  $$|
-$$__  $$ /$$__  $$| $$$ | $$←[0m
-←[38;2;0;100;255m                                | $$  \__/| $$  \ $$| $$  \ $$|
- $$  \ $$| $$  \ $$| $$$$| $$←[0m
-←[38;2;0;115;255m                                | $$      | $$$$$$$$| $$$$$$$/|
- $$$$$$$ | $$  | $$| $$ $$ $$←[0m
-←[38;2;0;130;255m                                | $$      | $$__  $$| $$__  $$|
- $$__  $$| $$  | $$| $$  $$$$←[0m
-←[38;2;0;145;255m                                | $$    $$| $$  | $$| $$  \ $$|
- $$  \ $$| $$  | $$| $$\  $$$←[0m
-←[38;2;0;160;255m                                |  $$$$$$/| $$  | $$| $$  | $$|
- $$$$$$$/|  $$$$$$/| $$ \  $$←[0m
-←[38;2;0;175;255m                                 \______/ |__/  |__/|__/  |__/|
-_______/  \______/ |__/  \__/←[0m
-←[38;2;0;190;255m←[0m
-←[38;2;0;205;255m←[0m
-←[38;2;0;220;255m←[0m
-←[38;2;0;235;255m        ←[38;2;43;0;220m[←[0m←[38;2;46;0;220m>←[0m←[38;2;49;0;2
-20m]←[0m←[38;2;52;0;220m ←[0m←[38;2;55;0;220mR←[0m←[38;2;58;0;220mu←[0m←[38;2;61
-;0;220mn←[0m←[38;2;64;0;220mn←[0m←[38;2;67;0;220mi←[0m←[38;2;70;0;220mn←[0m←[38;
-2;73;0;220mg←[0m←[38;2;76;0;220m ←[0m←[38;2;79;0;220mw←[0m←[38;2;82;0;220mi←[0m←
-[38;2;85;0;220mt←[0m←[38;2;88;0;220mh←[0m←[38;2;91;0;220m ←[0m←[38;2;94;0;220mP←
-[0m←[38;2;97;0;220my←[0m←[38;2;100;0;220mt←[0m←[38;2;103;0;220mh←[0m←[38;2;106;0
-;220mo←[0m←[38;2;109;0;220mn←[0m←[38;2;112;0;220m ←[0m←[38;2;115;0;220m3←[0m←[38
-;2;118;0;220m.←[0m←[38;2;121;0;220m6←[0m←[38;2;124;0;220m.←[0m←[38;2;127;0;220m6
-←[0m←[0m
+# Made by 0sir1ss @ https://github.com/0sir1ss/Carbon
+import ast, re, random, io, tokenize, os, sys, platform, math
 
-←[38;2;43;0;220m ←[0m←[38;2;46;0;220m ←[0m←[38;2;49;0;220m ←[0m←[38;2;52;0;220m
-←[0m←[38;2;55;0;220m ←[0m←[38;2;58;0;220m ←[0m←[38;2;61;0;220m ←[0m←[38;2;64;0;2
-20m ←[0m←[38;2;67;0;220m[←[0m←[38;2;70;0;220m>←[0m←[38;2;73;0;220m]←[0m←[38;2;76
-;0;220m ←[0m←[38;2;79;0;220mE←[0m←[38;2;82;0;220mn←[0m←[38;2;85;0;220mt←[0m←[38;
-2;88;0;220me←[0m←[38;2;91;0;220mr←[0m←[38;2;94;0;220m ←[0m←[38;2;97;0;220mt←[0m←
-[38;2;100;0;220mh←[0m←[38;2;103;0;220me←[0m←[38;2;106;0;220m ←[0m←[38;2;109;0;22
-0mp←[0m←[38;2;112;0;220my←[0m←[38;2;115;0;220mt←[0m←[38;2;118;0;220mh←[0m←[38;2;
-121;0;220mo←[0m←[38;2;124;0;220mn←[0m←[38;2;127;0;220m ←[0m←[38;2;130;0;220mf←[0
-m←[38;2;133;0;220mi←[0m←[38;2;136;0;220ml←[0m←[38;2;139;0;220me←[0m←[38;2;142;0;
-220m ←[0m←[38;2;145;0;220my←[0m←[38;2;148;0;220mo←[0m←[38;2;151;0;220mu←[0m←[38;
-2;154;0;220m ←[0m←[38;2;157;0;220mw←[0m←[38;2;160;0;220mi←[0m←[38;2;163;0;220ms←
-[0m←[38;2;166;0;220mh←[0m←[38;2;169;0;220m ←[0m←[38;2;172;0;220mt←[0m←[38;2;175;
-0;220mo←[0m←[38;2;178;0;220m ←[0m←[38;2;181;0;220mo←[0m←[38;2;184;0;220mb←[0m←[3
-8;2;187;0;220mf←[0m←[38;2;190;0;220mu←[0m←[38;2;193;0;220ms←[0m←[38;2;196;0;220m
-c←[0m←[38;2;199;0;220ma←[0m←[38;2;202;0;220mt←[0m←[38;2;205;0;220me←[0m←[38;2;20
-8;0;220m ←[0m←[38;2;211;0;220m[←[0m←[38;2;214;0;220ms←[0m←[38;2;217;0;220mc←[0m←
-[38;2;220;0;220mr←[0m←[38;2;223;0;220mi←[0m←[38;2;226;0;220mp←[0m←[38;2;229;0;22
-0mt←[0m←[38;2;232;0;220m.←[0m←[38;2;235;0;220mp←[0m←[38;2;238;0;220my←[0m←[38;2;
-241;0;220m]←[0m←[38;2;244;0;220m ←[0m←[38;2;247;0;220m:←[0m←[38;2;250;0;220m ←[0
-m←[38;2;148;0;230m
-←[38;2;0;10;255m←[0m
-←[38;2;0;25;255m←[0m
-←[38;2;0;40;255m←[0m
-←[38;2;0;55;255m←[0m
-←[38;2;0;70;255m                                  /$$$$$$   /$$$$$$  /$$$$$$$  /
-$$$$$$$   /$$$$$$  /$$   /$$←[0m
-←[38;2;0;85;255m                                 /$$__  $$ /$$__  $$| $$__  $$|
-$$__  $$ /$$__  $$| $$$ | $$←[0m
-←[38;2;0;100;255m                                | $$  \__/| $$  \ $$| $$  \ $$|
- $$  \ $$| $$  \ $$| $$$$| $$←[0m
-←[38;2;0;115;255m                                | $$      | $$$$$$$$| $$$$$$$/|
- $$$$$$$ | $$  | $$| $$ $$ $$←[0m
-←[38;2;0;130;255m                                | $$      | $$__  $$| $$__  $$|
- $$__  $$| $$  | $$| $$  $$$$←[0m
-←[38;2;0;145;255m                                | $$    $$| $$  | $$| $$  \ $$|
- $$  \ $$| $$  | $$| $$\  $$$←[0m
-←[38;2;0;160;255m                                |  $$$$$$/| $$  | $$| $$  | $$|
- $$$$$$$/|  $$$$$$/| $$ \  $$←[0m
-←[38;2;0;175;255m                                 \______/ |__/  |__/|__/  |__/|
-_______/  \______/ |__/  \__/←[0m
-←[38;2;0;190;255m←[0m
-←[38;2;0;205;255m←[0m
-←[38;2;0;220;255m←[0m
-←[38;2;0;235;255m        ←[38;2;43;0;220m[←[0m←[38;2;46;0;220m>←[0m←[38;2;49;0;2
-20m]←[0m←[38;2;52;0;220m ←[0m←[38;2;55;0;220mR←[0m←[38;2;58;0;220mu←[0m←[38;2;61
-;0;220mn←[0m←[38;2;64;0;220mn←[0m←[38;2;67;0;220mi←[0m←[38;2;70;0;220mn←[0m←[38;
-2;73;0;220mg←[0m←[38;2;76;0;220m ←[0m←[38;2;79;0;220mw←[0m←[38;2;82;0;220mi←[0m←
-[38;2;85;0;220mt←[0m←[38;2;88;0;220mh←[0m←[38;2;91;0;220m ←[0m←[38;2;94;0;220mP←
-[0m←[38;2;97;0;220my←[0m←[38;2;100;0;220mt←[0m←[38;2;103;0;220mh←[0m←[38;2;106;0
-;220mo←[0m←[38;2;109;0;220mn←[0m←[38;2;112;0;220m ←[0m←[38;2;115;0;220m3←[0m←[38
-;2;118;0;220m.←[0m←[38;2;121;0;220m6←[0m←[38;2;124;0;220m.←[0m←[38;2;127;0;220m6
-←[0m←[0m
+is_windows = True if platform.system() == "Windows" else False
 
-←[38;2;43;0;220m ←[0m←[38;2;46;0;220m ←[0m←[38;2;49;0;220m ←[0m←[38;2;52;0;220m
-←[0m←[38;2;55;0;220m ←[0m←[38;2;58;0;220m ←[0m←[38;2;61;0;220m ←[0m←[38;2;64;0;2
-20m ←[0m←[38;2;67;0;220m[←[0m←[38;2;70;0;220m>←[0m←[38;2;73;0;220m]←[0m←[38;2;76
-;0;220m ←[0m←[38;2;79;0;220mE←[0m←[38;2;82;0;220mn←[0m←[38;2;85;0;220mt←[0m←[38;
-2;88;0;220me←[0m←[38;2;91;0;220mr←[0m←[38;2;94;0;220m ←[0m←[38;2;97;0;220mt←[0m←
-[38;2;100;0;220mh←[0m←[38;2;103;0;220me←[0m←[38;2;106;0;220m ←[0m←[38;2;109;0;22
-0mp←[0m←[38;2;112;0;220my←[0m←[38;2;115;0;220mt←[0m←[38;2;118;0;220mh←[0m←[38;2;
-121;0;220mo←[0m←[38;2;124;0;220mn←[0m←[38;2;127;0;220m ←[0m←[38;2;130;0;220mf←[0
-m←[38;2;133;0;220mi←[0m←[38;2;136;0;220ml←[0m←[38;2;139;0;220me←[0m←[38;2;142;0;
-220m ←[0m←[38;2;145;0;220my←[0m←[38;2;148;0;220mo←[0m←[38;2;151;0;220mu←[0m←[38;
-2;154;0;220m ←[0m←[38;2;157;0;220mw←[0m←[38;2;160;0;220mi←[0m←[38;2;163;0;220ms←
-[0m←[38;2;166;0;220mh←[0m←[38;2;169;0;220m ←[0m←[38;2;172;0;220mt←[0m←[38;2;175;
-0;220mo←[0m←[38;2;178;0;220m ←[0m←[38;2;181;0;220mo←[0m←[38;2;184;0;220mb←[0m←[3
-8;2;187;0;220mf←[0m←[38;2;190;0;220mu←[0m←[38;2;193;0;220ms←[0m←[38;2;196;0;220m
-c←[0m←[38;2;199;0;220ma←[0m←[38;2;202;0;220mt←[0m←[38;2;205;0;220me←[0m←[38;2;20
-8;0;220m ←[0m←[38;2;211;0;220m[←[0m←[38;2;214;0;220ms←[0m←[38;2;217;0;220mc←[0m←
-[38;2;220;0;220mr←[0m←[38;2;223;0;220mi←[0m←[38;2;226;0;220mp←[0m←[38;2;229;0;22
-0mt←[0m←[38;2;232;0;220m.←[0m←[38;2;235;0;220mp←[0m←[38;2;238;0;220my←[0m←[38;2;
-241;0;220m]←[0m←[38;2;244;0;220m ←[0m←[38;2;247;0;220m:←[0m←[38;2;250;0;220m ←[0
-m←[38;2;148;0;230m
-←[38;2;255;245;0m ←[0m←[38;2;255;240;0m ←[0m←[38;2;255;235;0m ←[0m←[38;2;255;230
-;0m ←[0m←[38;2;255;225;0m ←[0m←[38;2;255;220;0m ←[0m←[38;2;255;215;0m ←[0m←[38;2
-;255;210;0m ←[0m←[38;2;255;205;0m[←[0m←[38;2;255;200;0m!←[0m←[38;2;255;195;0m]←[
-0m←[38;2;255;190;0m ←[0m←[38;2;255;185;0mE←[0m←[38;2;255;180;0mr←[0m←[38;2;255;1
-75;0mr←[0m←[38;2;255;170;0mo←[0m←[38;2;255;165;0mr←[0m←[38;2;255;160;0m ←[0m←[38
-;2;255;155;0m:←[0m←[38;2;255;150;0m ←[0m←[38;2;255;145;0mT←[0m←[38;2;255;140;0mh
-←[0m←[38;2;255;135;0ma←[0m←[38;2;255;130;0mt←[0m←[38;2;255;125;0m ←[0m←[38;2;255
-;120;0mf←[0m←[38;2;255;115;0mi←[0m←[38;2;255;110;0ml←[0m←[38;2;255;105;0me←[0m←[
-38;2;255;100;0m ←[0m←[38;2;255;95;0md←[0m←[38;2;255;90;0mo←[0m←[38;2;255;85;0me←
-[0m←[38;2;255;80;0ms←[0m←[38;2;255;75;0m ←[0m←[38;2;255;70;0mn←[0m←[38;2;255;65;
-0mo←[0m←[38;2;255;60;0mt←[0m←[38;2;255;55;0m ←[0m←[38;2;255;50;0me←[0m←[38;2;255
-;45;0mx←[0m←[38;2;255;40;0mi←[0m←[38;2;255;35;0ms←[0m←[38;2;255;30;0mt←[0m
-←[38;2;43;0;220m ←[0m←[38;2;46;0;220m ←[0m←[38;2;49;0;220m ←[0m←[38;2;52;0;220m
-←[0m←[38;2;55;0;220m ←[0m←[38;2;58;0;220m ←[0m←[38;2;61;0;220m ←[0m←[38;2;64;0;2
-20m ←[0m←[38;2;67;0;220m[←[0m←[38;2;70;0;220m>←[0m←[38;2;73;0;220m]←[0m←[38;2;76
-;0;220m ←[0m←[38;2;79;0;220mE←[0m←[38;2;82;0;220mn←[0m←[38;2;85;0;220mt←[0m←[38;
-2;88;0;220me←[0m←[38;2;91;0;220mr←[0m←[38;2;94;0;220m ←[0m←[38;2;97;0;220mt←[0m←
-[38;2;100;0;220mh←[0m←[38;2;103;0;220me←[0m←[38;2;106;0;220m ←[0m←[38;2;109;0;22
-0mp←[0m←[38;2;112;0;220my←[0m←[38;2;115;0;220mt←[0m←[38;2;118;0;220mh←[0m←[38;2;
-121;0;220mo←[0m←[38;2;124;0;220mn←[0m←[38;2;127;0;220m ←[0m←[38;2;130;0;220mf←[0
-m←[38;2;133;0;220mi←[0m←[38;2;136;0;220ml←[0m←[38;2;139;0;220me←[0m←[38;2;142;0;
-220m ←[0m←[38;2;145;0;220my←[0m←[38;2;148;0;220mo←[0m←[38;2;151;0;220mu←[0m←[38;
-2;154;0;220m ←[0m←[38;2;157;0;220mw←[0m←[38;2;160;0;220mi←[0m←[38;2;163;0;220ms←
-[0m←[38;2;166;0;220mh←[0m←[38;2;169;0;220m ←[0m←[38;2;172;0;220mt←[0m←[38;2;175;
-0;220mo←[0m←[38;2;178;0;220m ←[0m←[38;2;181;0;220mo←[0m←[38;2;184;0;220mb←[0m←[3
-8;2;187;0;220mf←[0m←[38;2;190;0;220mu←[0m←[38;2;193;0;220ms←[0m←[38;2;196;0;220m
-c←[0m←[38;2;199;0;220ma←[0m←[38;2;202;0;220mt←[0m←[38;2;205;0;220me←[0m←[38;2;20
-8;0;220m ←[0m←[38;2;211;0;220m[←[0m←[38;2;214;0;220ms←[0m←[38;2;217;0;220mc←[0m←
-[38;2;220;0;220mr←[0m←[38;2;223;0;220mi←[0m←[38;2;226;0;220mp←[0m←[38;2;229;0;22
-0mt←[0m←[38;2;232;0;220m.←[0m←[38;2;235;0;220mp←[0m←[38;2;238;0;220my←[0m←[38;2;
-241;0;220m]←[0m←[38;2;244;0;220m ←[0m←[38;2;247;0;220m:←[0m←[38;2;250;0;220m ←[0
-m←[38;2;148;0;230m
+if is_windows:
+    os.system("title Carbon @ github.com/0sir1ss/Carbon")
+
+def clear():
+    if is_windows:
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def pause():
+    if is_windows:
+        os.system(f"pause >nul")
+    else:
+        input()
+
+def leave():
+    try:
+        sys.exit()
+    except:
+        exit()
+
+def error(error):
+    print(red(f"        [>] Error: {error}"), end="")
+    pause(); clear(); leave()
+
+def red(text):
+    os.system(""); faded = ""
+    for line in text.splitlines():
+        green = 250
+        for character in line:
+            green -= 5
+            if green < 0:
+                green = 0
+            faded += (f"\033[38;2;255;{green};0m{character}\033[0m")
+        faded += "\n"
+    return faded
+
+def blue(text):
+    os.system(""); faded = ""
+    for line in text.splitlines():
+        green = 0
+        for character in line:
+            green += 3
+            if green > 255:
+                green = 255
+            faded += (f"\033[38;2;0;{green};255m{character}\033[0m")
+        faded += "\n"
+    return faded
+
+def water(text):
+    os.system(""); faded = ""
+    green = 10
+    for line in text.splitlines():
+        faded += (f"\033[38;2;0;{green};255m{line}\033[0m\n")
+        if not green == 255:
+            green += 15
+            if green > 255:
+                green = 255
+    return faded
+
+def purple(text):
+    os.system("")
+    faded = ""
+    down = False
+
+    for line in text.splitlines():
+        red = 40
+        for character in line:
+            if down:
+                red -= 3
+            else:
+                red += 3
+            if red > 254:
+                red = 255
+                down = True
+            elif red < 1:
+                red = 30
+                down = False
+            faded += (f"\033[38;2;{red};0;220m{character}\033[0m")
+    return faded
+
+def remove_docs(source):
+    io_obj = io.StringIO(source)
+    out = ""
+    prev_toktype = tokenize.INDENT
+    last_lineno = -1
+    last_col = 0
+    for tok in tokenize.generate_tokens(io_obj.readline):
+        token_type = tok[0]
+        token_string = tok[1]
+        start_line, start_col = tok[2]
+        end_line, end_col = tok[3]
+        if start_line > last_lineno:
+            last_col = 0
+        if start_col > last_col:
+            out += (" " * (start_col - last_col))
+        if token_type == tokenize.COMMENT:
+            pass
+        elif token_type == tokenize.STRING:
+            if prev_toktype != tokenize.INDENT:
+                if prev_toktype != tokenize.NEWLINE:
+                    if start_col > 0:
+                        out += token_string
+        else:
+            out += token_string
+        prev_toktype = token_type
+        last_col = end_col
+        last_lineno = end_line
+    out = '\n'.join(l for l in out.splitlines() if l.strip())
+    return out
+
+def do_rename(pairs, code):
+    for key in pairs:
+        code = re.sub(fr"\b({key})\b", pairs[key], code, re.MULTILINE)
+    return code
+
+def rename(filename: str):
+    with open(filename, "r") as file:
+        code = file.read()
+        code = remove_docs(code)
+        parsed = ast.parse(code)
+
+    funcs = {
+        node for node in ast.walk(parsed) if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef))
+    }
+    classes = {
+        node for node in ast.walk(parsed) if isinstance(node, ast.ClassDef)
+    }
+    args = {
+        node.id for node in ast.walk(parsed) if isinstance(node, ast.Name) and not isinstance(node.ctx, ast.Load)
+    }
+    attrs = {
+        node.attr for node in ast.walk(parsed) if isinstance(node, ast.Attribute) and not isinstance(node.ctx, ast.Load)
+    }
+    for func in funcs:
+        if func.args.args:
+            for arg in func.args.args:
+                args.add(arg.arg)
+        if func.args.kwonlyargs:
+            for arg in func.args.kwonlyargs:
+                args.add(arg.arg)
+        if func.args.vararg:
+            args.add(func.args.vararg.arg)
+        if func.args.kwarg:
+            args.add(func.args.kwarg.arg)
+
+    pairs = {}
+    used = set()
+    for func in funcs:
+        if func.name == "__init__":
+            continue
+        newname = "".join(random.choice(["I", "l"]) for i in range(random.randint(8, 20)))
+        while newname in used:
+            newname = "".join(random.choice(["I", "l"]) for i in range(random.randint(8, 20)))
+        used.add(newname)
+        pairs[func.name] = newname
+
+    for _class in classes:
+        newname = "".join(random.choice(["I", "l"]) for i in range(random.randint(8, 20)))
+        while newname in used:
+            newname = "".join(random.choice(["I", "l"]) for i in range(random.randint(8, 20)))
+        used.add(newname)
+        pairs[_class.name] = newname
+
+    for arg in args:
+        newname = "".join(random.choice(["I", "l"]) for i in range(random.randint(8, 20)))
+        while newname in used:
+            newname = "".join(random.choice(["I", "l"]) for i in range(random.randint(8, 20)))
+        used.add(newname)
+        pairs[arg] = newname
+
+    for attr in attrs:
+        newname = "".join(random.choice(["I", "l"]) for i in range(random.randint(8, 20)))
+        while newname in used:
+            newname = "".join(random.choice(["I", "l"]) for i in range(random.randint(8, 20)))
+        used.add(newname)
+        pairs[attr] = newname
+
+    string_regex = r"('|\")[\x1f-\x7e]{1,}?('|\")"
+
+    original_strings = re.finditer(string_regex, code, re.MULTILINE)
+    originals = []
+
+    for matchNum, match in enumerate(original_strings, start=1):
+        originals.append(match.group().replace("\\", "\\\\"))
+
+    placeholder = os.urandom(16).hex()
+    code = re.sub(string_regex, f"'{placeholder}'", code, 0, re.MULTILINE)
+
+    for i in range(len(originals)):
+        for key in pairs:
+            originals[i] = re.sub(r"({.*)(" + key + r")(.*})", "\\1" + pairs[key] + "\\3", originals[i], re.MULTILINE)
+
+    while True:
+        found = False
+        code = do_rename(pairs, code)
+        for key in pairs:
+            if re.findall(fr"\b({key})\b", code):
+                found = True
+        if found == False:
+            break
+
+
+    replace_placeholder = r"('|\")" + placeholder + r"('|\")"
+    for original in originals:
+        code = re.sub(replace_placeholder, original, code, 1, re.MULTILINE)
+
+    return code
+
+banner = f"""
+
+
+
+                                  /$$$$$$   /$$$$$$  /$$$$$$$  /$$$$$$$   /$$$$$$  /$$   /$$
+                                 /$$__  $$ /$$__  $$| $$__  $$| $$__  $$ /$$__  $$| $$$ | $$
+                                | $$  \__/| $$  \ $$| $$  \ $$| $$  \ $$| $$  \ $$| $$$$| $$
+                                | $$      | $$$$$$$$| $$$$$$$/| $$$$$$$ | $$  | $$| $$ $$ $$
+                                | $$      | $$__  $$| $$__  $$| $$__  $$| $$  | $$| $$  $$$$
+                                | $$    $$| $$  | $$| $$  \ $$| $$  \ $$| $$  | $$| $$\  $$$
+                                |  $$$$$$/| $$  | $$| $$  | $$| $$$$$$$/|  $$$$$$/| $$ \  $$
+                                 \______/ |__/  |__/|__/  |__/|_______/  \______/ |__/  \__/
+
+
+
+        {purple(f"[>] Running with Python {sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}")}
+"""
+clear()
+print(water(banner))
+while True:
+    file = input(purple("        [>] Enter the python file you wish to obfuscate [script.py] : ") + "\033[38;2;148;0;230m")
+    if not os.path.exists(file):
+        print(red("        [!] Error : That file does not exist"), end="")
+    else:
+        break
+
+code = rename(file)
+
+with open(f"{file[:-3]}-obf.py", "w") as f:
+    f.write(code)
+
+print(blue(f"        [>] Code has been successfully obfuscated @ {file[:-3]}-obf.py"), end="")
+pause(); clear(); leave()
